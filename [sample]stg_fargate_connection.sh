a7d1220ed34c6783e03e6ef6a646b72b73c752ff
jin <jin@metaps-payment.com>
2025-06-20 12:28:02 +0900

    fargate接続ツールをvenv環境で実行する

---
+++ [sample]stg_fargate_connection.sh
@@ -0,0 +1,10 @@
+python3 -m venv ./venv
+source ./venv/bin/activate
+if command -v pip &> /dev/null; then
+    pip install -r requirements.txt
+else
+    pip3 install -r requirements.txt
+fi
+export AWS_PROFILE=test_profile_name;aws sso login
+mkdir -p ~/.connect_to_fargate/log
+./connect_to_fargate.py "$@"
--- [sample]test_server_connection.sh
+++
@@ -1,5 +0,0 @@
-python3 -m venv path/to/venv
-source path/to/venv/bin/activate
-pip3 install -r requirements.txt
-export AWS_PROFILE=test_profile_name;aws sso login
-connect_to_fargate.py
