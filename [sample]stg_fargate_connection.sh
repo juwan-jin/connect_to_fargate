52fcd36f3f48a52b32157cac1367533375ddf2f3
jin <jin@metaps-payment.com>
2025-06-23 15:59:33 +0900

    symbolicファイルの原本のPATHで実行できるようにする

--- [sample]stg_fargate_connection.sh
+++ [sample]stg_fargate_connection.sh
@@ -1,10 +1,15 @@
+SCRIPT_PATH="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 python3 -m venv ./venv
 source ./venv/bin/activate
 if command -v pip &> /dev/null; then
-    pip install -r requirements.txt
+    (cd "$SCRIPT_DIR" && pip install -r requirements.txt)
 else
-    pip3 install -r requirements.txt
+    (cd "$SCRIPT_DIR" && pip3 install -r requirements.txt)
 fi
 export AWS_PROFILE=stg_profile_name;aws sso login
 mkdir -p ~/.connect_to_fargate/log
-./connect_to_fargate.py "$@"
+
+SCRIPT_PATH="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+(cd "$SCRIPT_DIR" && connect_to_fargate.py "$@")
